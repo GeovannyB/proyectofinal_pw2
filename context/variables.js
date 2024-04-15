6c837d4b7506c40ebe352606e065a6d57a8971c2
elvinhray <elvinhray@gmail.com>
2024-04-14 18:44:56 -0600

    nuevo index

--- context/variables.js
+++ context/variables.js
@@ -1,14 +1,14 @@
 import prodsPage from './productosPage';
 import galPage from './galeriaPage';
 import blogPage from './blogPage';
+import indexPage from './indexPage';
 
 const pageContext = {
     "/productos.html":prodsPage,
     "/galeria.html":galPage,
     "/blog.html":blogPage,
-    
+    "/index.html":indexPage
 }
-
 
 const getContext = (contextKey) => {
     const _pageContext = pageContext[contextKey] || {};
